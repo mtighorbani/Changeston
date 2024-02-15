f0c5c8c235ad60dcdbf9e81d78a20760bf5c3cc6
My first redux project <mahdi1381.ghorbani@gmail.com>
2024-02-15 19:23:02 +0330

    photo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import LogInButton from "./LogInButton";
 import ThemeSwitch from "./Theme";
-
-
+import './cssStyle/font.css'
 const Navbar = () => {
   return (
-    <div className=" flex justify-between px-20 py-5 max-h-[100px]">
-      
+    <>
+    <div className=" flex justify-between px-20 py-5 max-h-[100px] ">
       <div className="flex w-1/3">
-        <LogInButton/>
+        <LogInButton />
         <ThemeSwitch />
       </div>
-      <div className="flex-end" ></div>
+      <div className="flex justify-between pt-2 font-bold font">
+        <span className="px-4 "><a href="/">درباره فروشگاه</a></span>
+        <span className="px-4 "><a href="/">تماس با ما</a></span>
+        <span className="px-4 "><a href="/">قوانین خرید</a></span>
+        <p className="px-4">Changeston</p>
+
+      </div>
     </div>
+   <div className="w-5/6 h-1 bg-gray-500 m-auto"/>
+   </>
   );
 };
 
