a2cec026ad658796d860040149baa41294ccb424
My first redux project <mahdi1381.ghorbani@gmail.com>
2024-02-16 00:23:23 +0330

    BoxChanges

--- components/PurchesRoute.tsx
+++ components/PurchesRoute.tsx
@@ -19,14 +19,14 @@ const purchesStep = [
 
 const PurchesRoute = () => {
   const PurchesStepBox = purchesStep.map(step=> 
-    <div className="pt-6  px-12 text-black place-content-center " key={step.id}>
+    <div className="pt-4  px-12 text-black place-content-center " key={step.id}>
       <div className="w-14 h-14 text-center pt-3  text-white m-auto  rounded-lg bg-[#5a8dee]">{step.id}</div>
       <p className="mt-3 ">{step.name}</p>
     </div>
   );
 
   return (
-    <div className=" flex justify-center w-[40%] h-[17%] top-[70%] absolute left-[28%] ring-4 ring-[#5a8dee] rounded-2xl  bg-gray-100 shadow-2lg shadow-gray-100/50 ">
+    <div className=" flex justify-center w-[38%] h-[15%] top-[70%] absolute left-[30%] ring-4 ring-[#5a8dee] rounded-2xl  bg-gray-100 shadow-2lg shadow-gray-100/50 ">
       {PurchesStepBox}
     </div>
   );
