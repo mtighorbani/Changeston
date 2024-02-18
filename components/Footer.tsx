9a44bf27c0b6f04bce703840d3807d07ac67c872
My first redux project <mahdi1381.ghorbani@gmail.com>
2024-02-18 21:52:16 +0330

    footer darkMode

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Imafe from '../asset/images/Enamadlogo.png'
 
 const Footer = () => {
   return (
-    <footer className=' flex mb-0 justify-between w-full h-[20%] py-10 bg-slate-100'>
+    <footer className=' dark:bg-black flex mb-0 justify-between w-full h-[20%] py-10 bg-slate-100'>
         <div className='ml-[10%] my-auto'>کلیه حقوق این سایت متعلق به چنجستون می باشد.</div>
         <div className='mr-[10%] my-auto'> <Image src={Imafe} alt='Enamad' /></div>
     </footer>
