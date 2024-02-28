c0c5820ccb84018429ae5bd51c38e87cb86536e5
Zohreh Pourfarzam <z.pourfarzam@flightio.com>
2024-02-28 12:42:25 +0330

    EnamadUpdated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,19 +4,13 @@ import Imafe from "../asset/images/Enamadlogo.png";
 
 const Footer = () => {
   return (
-    <footer dir="rtl" className=" dark:bg-black flex max-sm:block mb-0 justify-between w-full h-[20%] py-10 bg-slate-100">
+    <footer dir="rtl" className=" dark:bg-black flex  mb-0 justify-between w-full h-[20%] py-10 bg-slate-100">
+        <div dir="rtl" className="mr-[10%] max-sm:mt-12 max-sm:mr-4 my-auto">
 
-      <div dir="rtl" className="mr-[10%] my-auto block">
-        {" "}
-        <a className="block font-bold pb-2 text-lg" href="/">
-          تماس با ما
-        </a>
-        <a className="block font-bold pb-2 text-lg" href="/">
-          درباره ما
-        </a>
-        <a className="block font-bold pb-2 text-lg" href="/"> قوانین خرید</a>
+      <a referrerPolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=467241&Code=PUl45fNIVUi5QYQsxxnunJ0XD1sIpjqT'><Image referrerPolicy='origin' src={Imafe} alt='' className='curcursor-pointer w-28 h-28'width={0} height={0} /></a>  
       </div>
-      <div dir="rtl" className="ml-[10%] max-sm:mt-4 max-sm:mr-4 my-auto">
+
+     <div dir="rtl" className="ml-[10%] max-sm:mt-12 max-sm:mr-4 my-auto">
         کلیه حقوق این سایت متعلق به چنجستون می باشد.
       </div>
     </footer>
