c8104097fe890368620b5fe8c19974331c7e256e
Majid-Ebrahimi <majidebrahiimi@gmail.com>
2024-03-10 15:37:23 +0330

    ReactQueryDevtools added

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "next-themes";
 
 // Create a client
@@ -10,7 +11,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     // Provide the client to your App
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
