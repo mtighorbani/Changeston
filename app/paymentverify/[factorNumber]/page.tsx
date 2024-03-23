88fb2e8cd29278cdefab590023899e238616bb12
Majid-Ebrahimi <majidebrahiimi@gmail.com>
2024-03-23 18:30:36 +0330

    named url added

--- app/paymentverify/[factorNumber]/page.tsx
+++ app/paymentverify/[factorNumber]/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 import { errorMessage } from "@/global/errorMessage";
 import { paymentValidate } from "@/global/urls";
+import { useQuery } from "@tanstack/react-query";
+import { Spin } from "antd";
 import axios from "axios";
 import { useParams, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const PaymentVerify = () => {
-  const [paymentStatus, setPaymentStatus] = useState<boolean>(false);
+  // const [paymentStatus, setPaymentStatus] = useState<boolean>(false);
   // شماره فاکتور
   const params = useParams<{ factorNumber: string }>();
 
   const searchParams = useSearchParams();
-  const Success = searchParams.get("success");
-  const Status = searchParams.get("status");
+  // const Success = searchParams.get("success");
+  // const Status = searchParams.get("status");
   const TrackId = searchParams.get("trackId");
 
-  const paymentValidationCheck = async () => {
+  /*   const paymentValidationCheck = async () => {
     try {
       const response = await axios.get(`${paymentValidate}${TrackId}`);
       setPaymentStatus(response.data.success);
@@ -23,21 +25,32 @@ const PaymentVerify = () => {
     } catch (error) {
       console.error(error);
     }
-  };
-  useEffect(() => {
-    paymentValidationCheck();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }; */
 
-  return paymentStatus ? (
+  const {
+    data: paymentValidateData,
+    refetch: refetchPaymentValidate,
+    isFetching: isFetchingPaymentValidate,
+  } = useQuery({
+    queryFn: async () => (await axios.get(`${paymentValidate}${TrackId}`)).data,
+    queryKey: ["paymentValidateData"],
+    enabled: false,
+  });
+  useEffect(() => {
+    // paymentValidationCheck();
+    refetchPaymentValidate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [TrackId]);
+
+  return isFetchingPaymentValidate ? (
+    <Spin />
+  ) : paymentValidateData ? (
     <div>
-      Success!!! success: {Success}, status: {Status}, trackId: {TrackId},
-      factorNumber: {params.factorNumber}
+      Success!!! trackId: {TrackId}, factorNumber: {params.factorNumber}
     </div>
   ) : (
     <div>
-      Failed!!! success: {Success}, status: {Status}, trackId: {TrackId},
-      factorNumber: {params.factorNumber}
+      Failed!!! trackId: {TrackId}, factorNumber: {params.factorNumber}
     </div>
   );
 };
---
+++ app/wisev2/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import CryptoPurchaseForm from "@/components/CryptoPurchesForm";
+import { useParams, useSearchParams } from "next/navigation";
+
+const PayV2 = () => {
+  const searchParams = useSearchParams();
+  const receiver_name = searchParams.get("receiver_name");
+  const currency_type = searchParams.get("currency_type");
+  const amount = searchParams.get("amount");
+  const receiver_email = searchParams.get("receiver_email");
+  const iban = searchParams.get("iban");
+
+  return (
+    <CryptoPurchaseForm
+      amount={amount}
+      currency_type={currency_type || ""}
+      group_id="4"
+      iban={iban || ""}
+      payment_method="zibal"
+      receiver_email={receiver_email || ""}
+      receiver_name={receiver_name || ""}
+    />
+  );
+};
+
+export default PayV2;
