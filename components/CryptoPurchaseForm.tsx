25274a0151a526448875f993de63103edd60422d
Majid-Ebrahimi <majidebrahiimi@gmail.com>
2024-05-10 20:58:58 +0330

    fix: rule checkbox error

--- components/CryptoPurchaseForm.tsx
+++ components/CryptoPurchaseForm.tsx
@@ -145,11 +145,12 @@ const CryptoPurchaseForm = (props: PurchasePostData) => {
             <div className="flex items-center mt-4">
               <input
                 id="RulConfirm"
-                type="radio"
+                type="checkbox"
                 name="ruls"
-                value="confirm"
-                className="w-4 h-4 border-gray-300 mr-3 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
                 checked
+                value="confirm"
+                readOnly
+                className="w-4 h-4 border-gray-300 mr-3 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
               />
               <label
                 typeof="country-option-1"
