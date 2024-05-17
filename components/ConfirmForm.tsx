bb05889e064549cd0634844b2b74ec952cf69b44
Majid-Ebrahimi <majidebrahiimi@gmail.com>
2024-05-17 18:07:00 +0330

    verified panels api added

--- components/ConfirmForm.tsx
+++ components/ConfirmForm.tsx
@@ -1,29 +1,33 @@
 import Image, { StaticImageData } from "next/image";
 import React from "react";
 
-
-
 interface FormProps {
-  imgUrl: StaticImageData
-  price: string
-  FaName:string
-  purchaseId:number
+  id: number | undefined;
+  amount: number | undefined;
+  name: string | undefined;
+  imgUrl?: StaticImageData;
 }
 
-const ConfirmForm = ({imgUrl,price,FaName, purchaseId: purchaseId }:FormProps) => {
-  const LogInErrHandler = ()=>{
-    alert('با عرض پوزش در حال حاضر امکان خرید و ساخت اکانت وجود ندارد')
-  }
-
+const ConfirmForm = ({
+  imgUrl,
+  amount: amount,
+  name: name,
+  id: id,
+}: FormProps) => {
+  const LogInErrHandler = () => {
+    alert("با عرض پوزش در حال حاضر امکان خرید و ساخت اکانت وجود ندارد");
+  };
 
   return (
-    <div dir="rtl" className="flex max-sm:grid max-sm:grid-cols-1 justify-between h-[60%] mt-20 w-full mb-30">
+    <div
+      dir="rtl"
+      className="flex max-sm:grid max-sm:grid-cols-1 justify-between h-[60%] mt-20 w-full mb-30"
+    >
       <div className="w-[35%] text-black max-sm:w-full max-sm:mr-0 rounded-xl grid grid-cols-1 min-h-[400px] dark:bg-black   bg-[#EEEEEE] mt-4 max-h=[700px] mr-[14%]">
         <form className="max-w-sm mr-3 mt-4 ">
           <label
-            typeof="email" 
+            typeof="email"
             className="block  dark:text-white  rtl:mr-0 mt-2 h-[10%] text-sm font-bold mb-1 text-gray-900 "
-            
           >
             <p dir="rtl" className="text-right  ">
               {" "}
@@ -39,7 +43,6 @@ const ConfirmForm = ({imgUrl,price,FaName, purchaseId: purchaseId }:FormProps) =
             minLength={11}
           />
           <label
-          
             typeof="number"
             className="block dark:text-white  rtl:mr-0 mt-2 h-[10%] text-sm  font-bold mb-1 text-gray-900 "
           >
@@ -75,27 +78,38 @@ const ConfirmForm = ({imgUrl,price,FaName, purchaseId: purchaseId }:FormProps) =
           />
         </form>
         <div className="w-full h-12 max-sm:h-16 mt-5 text-white pr-3 pt-[9px] rounded-lg font-bold  bg-gradient-to-r from-[#C8338C] to-[#0A95E5] btn-primary">
-          
           در هنگام ورود شماره همراه و ایمیل دقت کنید، اطلاعات خرید به آنها ارسال
           می شود.
         </div>
       </div>
-      <div key={purchaseId}  className="w-[35%] rounded-xl max-sm:w-full max-sm:pb-6  min-h-[300px] dark:bg-black bg-[#EEEEEE] mt-4 max-h=[700px] ml-[14%] ">
-        <div  className="bg-[#F9FAFB] dark:bg-[#374151] w-full h-32 mt-8 rounded-xl ">
-
-          <div key={purchaseId} className="flex justify-between">
-            <Image src={imgUrl} alt={FaName} className="h-[80%] max-sm:pt-7 pr-4 w-[30%] rounded-sm pt-3 relative"/>
-            <p className="font-extrabold text-xl m-auto max-sm:pt-7">{FaName}</p>
-
-          </div>
-
+      <div
+        key={id}
+        className="w-[35%] rounded-xl max-sm:w-full max-sm:pb-6  min-h-[300px] dark:bg-black bg-[#EEEEEE] mt-4 max-h=[700px] ml-[14%] "
+      >
+        <div className="bg-[#F9FAFB] dark:bg-[#374151] w-full h-32 mt-8 rounded-xl ">
+          {
+            // TODO : remove ti=his condition when images added
+            <div key={id} className="flex justify-between">
+              <Image
+                src={imgUrl || "/images/empty.jpg"}
+                width={300}
+                height={200}
+                alt={name || ""}
+                className="h-[80%] max-sm:pt-7 pr-4 w-[30%] rounded-sm pt-3 relative"
+              />
+              <p className="font-extrabold text-xl m-auto max-sm:pt-7">
+                {name}
+              </p>
+            </div>
+          }
         </div>
         <div className="bg-[#F9FAFB] dark:bg-[#374151] w-full h-14 mt-5 rounded-xl">
-          <p  className="text-cente pt-3 pr-4 font-extrabold text-xl">جمع مبلغ : {price}</p>
+          <p className="text-cente pt-3 pr-4 font-extrabold text-xl">
+            جمع مبلغ : {amount}
+          </p>
         </div>
         <div>
           <div className="flex items-center mt-4">
-            
             <input
               id="RulConfirm"
               type="radio"
@@ -109,16 +123,20 @@ const ConfirmForm = ({imgUrl,price,FaName, purchaseId: purchaseId }:FormProps) =
               typeof="country-option-1"
               className="block ms-2   text-sm font-medium text-gray-900 dark:text-gray-300 "
             >
-              قوانین و ضوابط سایت را مطالعه کرده و می پذیرم. مشاهده قوانین و ضوابط
+              قوانین و ضوابط سایت را مطالعه کرده و می پذیرم. مشاهده قوانین و
+              ضوابط
             </label>
           </div>
         </div>
-        <div onClick={LogInErrHandler} className="w-[30%]  text-center cursor-not-allowed h-10 text-white pr-3 pt-[7px] mt-10 mr-6  rounded-lg font-bold bg-gradient-to-r from-[#C8338C] to-[#0A95E5]  ">
+        <div
+          onClick={LogInErrHandler}
+          className="w-[30%]  text-center cursor-not-allowed h-10 text-white pr-3 pt-[7px] mt-10 mr-6  rounded-lg font-bold bg-gradient-to-r from-[#C8338C] to-[#0A95E5]  "
+        >
           تایید و پرداخت
         </div>
       </div>
     </div>
-  )}
-
+  );
+};
 
 export default ConfirmForm;
---
+++ components/ProductsList.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { verifiedPanelsListUrl } from "@/global/urls";
+import { VerifiedPanels, VerifiedPanelsListResponse } from "@/models/models";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import ConfirmForm from "./ConfirmForm";
+import VerifiedPanelsProduct from "./VerifiedPanelsProduct";
+import { Spin } from "antd";
+
+interface Props {
+  productId: number;
+  roadMapIdSetter: (stepId: number) => any;
+}
+
+const ProductList = (props: Props) => {
+  // ** States
+  const [purchaseVisible, setPurchaseVisible] = useState(false);
+  const [selectedProductForPurchase, setSelectedProductForPurchase] =
+    useState<VerifiedPanels>();
+
+  // ** API Calls
+  const {
+    data: verifiedPanelsList,
+    isFetching: isFetchingVerifiedPanelsList,
+  } = useQuery<VerifiedPanelsListResponse>({
+    queryFn: async () => (await axios.get(verifiedPanelsListUrl)).data,
+    queryKey: ["verifiedPanelsList"],
+    retry: false,
+  });
+
+  // ** Handlers
+  const purchaseVerifiedPanelsHandler = (item: VerifiedPanels) => {
+    setPurchaseVisible(true);
+    setSelectedProductForPurchase(item);
+    props.roadMapIdSetter(3);
+    // SetImgUrl(item.Photo);
+  };
+
+  return purchaseVisible ? (
+    <ConfirmForm
+      id={selectedProductForPurchase?.id}
+      amount={selectedProductForPurchase?.amount}
+      name={selectedProductForPurchase?.name}
+    />
+  ) : (
+    <div
+      className={
+        "max-sm:grid-cols-1 grid grid-cols-4 justify-between place-content-center max-sm:mx-20 mx-[10%] max-sm:mt-20  mt-28 gap-4  "
+      }
+    >
+      {isFetchingVerifiedPanelsList ? (
+        <Spin />
+      ) : props.productId === 5 ? (
+        verifiedPanelsList?.verfiedpanelsgroup?.map((item) => {
+          return (
+            <VerifiedPanelsProduct
+              key={item.id}
+              Item={item}
+              onClick={() => {
+                return purchaseVerifiedPanelsHandler(item);
+              }}
+            />
+          );
+        })
+      ) : (
+        <div>gift card</div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
